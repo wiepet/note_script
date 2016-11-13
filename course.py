fedeb711ed0591c88b625807cb8479bac4e86dd9
Peter Wied <peter.w>
2016-11-13 21:52:37 +0100

    Version 0.0.1
    Finished initial draft of program!
    TODO:
    - debug

--- course.py
+++ course.py
@@ -3,16 +3,21 @@
 
 import os
 import datetime
+import subprocess
 
 class Course:
     """Creates a course object.
 
     Attributes: name of the course."""
-    def __init__(self, home_directory, name, teacher):
+    def __init__(self, home_directory, name):
         self.home_directory=home_directory
         self.name=name
-        self.teacher=teacher
-        directory = directory + "/" + name
+        directory = home_directory + "/" + name
+        note_file = directory + "/notes" + name + ".md"
+
+    def current_time():
+        now = datetime.datetime.now()
+        time = now.strftime("%Y-%m-%d %H:%M")
 
     def create():
         """Create the course folders notes, slides"""
@@ -24,12 +29,9 @@ class Course:
         header = header_file.read()
         header_file.close()
 
-        now = datetime.datetime.now()
-        time = now.strftime("%Y-%m-%d %H:%M")
-
-        note_file = open(directory + "/notes/" + name + ".md", "c")
-        note_file.write(header.format(name.title(), time, teacher))
-        note_file.close()
+        note_in = open(note_file)
+        note_in.write(header.format(name.title(), current_time()))
+        note_in.close()
 
     def work():
         """Appends date and time to notes file and opens it using VIM"""
@@ -37,14 +39,12 @@ class Course:
         header = work_layout_file.read()
         work_layout_file.close()
 
-        now = datetime.datetime.now()
-        time = now.strftime("%Y-%m-%d %H:%M")
-
         note_file = open(directory + "/notes/" + name + ".md", "a")
-        note_file.write(header.format(time, teacher))
+        note_file.write(header.format(current_time()))
         note_file.close()
 
-        # TODO: Create subprocess for VIM
+        subprocess.run("vim " + note_file)
 
     def copy_from_download():
         """Copys all files from download folder to slides"""
+        pass
--- uni.py
+++ uni.py
@@ -2,11 +2,85 @@
 # coding=utf-8
 
 import os
-import sys
+import course
 
 UNI_DIR = os.getenv("HOME") + "/Documents/uni"
+ALL_FOLDERS = os.listdir(UNI-DIR)
+COUSE_FOLDERS = []
 
-if os.path.exists(UNI_DIR + "/" + course):
-    pass
-else:
-    course.create()
+def start():
+    """Initiate program.
+
+    Also checks if the uni directory exists."""
+    if os.path.exists(UNI_DIR):
+        break
+    else:
+        print("""It appears your uni folder does not exist.
+        Would you like to create one?[y/n]""")
+        user_in = input("Enter command: ")
+        if user_in == "y" or "yes":
+            os.mkdir(UNI_DIR)
+        elif user_in == "n" or "no":
+            print("As you wish sir. Goodbye!")
+            quit()
+        else:
+            print("Sorry please answer with [y]es or [n]o!")
+            start()
+
+def goodbye():
+    """Prompt for quitiing."""
+    prompt = "Are you sure you would like to quit sir? [y/n]"
+    user_in = input("Enter command: ")
+    if user_in == y:
+        quit()
+    elif user_in == n:
+        break
+    else:
+        print("Sorry I don't know that command.")
+        goodbye()
+
+def list_courses():
+    for x in COURSE_FOLDERS:
+        print(x)
+
+print("Welcome sir!")
+
+start()
+
+# Only select non hidden folders
+for x in ALL_FOLDERS:
+    if not x.startswith(".")
+        COURSE_FOLDERS.append(x)
+
+# Create a Course object for each folder in uni
+COURSES = {}
+for x in COURSE_FOlDERS:
+    COURSES[x] = Course(UNI_DIR, x)
+
+while True:
+    print("How can I help you sir?")
+    USER_IN = input("Enter command: ")
+    if USER_IN == "l":
+        list_courses()
+    elif USER_IN == "c":
+        COURSE_NAME = input("Enter course name: ")
+        COURSES[COURSE_NAME] = Course(UNI_DIR, COURSE_NAME)
+    elif USER_IN == "w":
+        print("Which course would you like to work on?")
+        list_courses()
+        WORK_ON = input("Enter course name: ")
+        for x in COURSES[x]:
+            if x == WORK_ON:
+                COURSES[x].work()
+    elif USER_IN == "q":
+        goodbye()
+    else:
+        print("Sorry I don't know that command sir."
+        print("""[l] - list courses
+        [c] - create new course
+        [w] - work on existing course
+        [q] - quit program"""
+
+
+
+
