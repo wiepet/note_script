01bb32adaecc4785923a83e9ec30108e83cf6abf
Peter Wied <peter.w>
2016-11-13 23:59:12 +0100

    Release Version 0.1.1
    Many synthax errors fixed:)

--- course.py
+++ course.py
@@ -10,41 +10,42 @@ class Course:
 
     Attributes: name of the course."""
     def __init__(self, home_directory, name):
-        self.home_directory=home_directory
-        self.name=name
-        directory = home_directory + "/" + name
-        note_file = directory + "/notes" + name + ".md"
+        self.home_directory = home_directory
+        self.name = name
+        Course.directory = home_directory + "/" + name
+        Course.note_file = Course.directory + "/notes/" + name + ".md"
 
-    def current_time():
+    def current_time(self):
         now = datetime.datetime.now()
         time = now.strftime("%Y-%m-%d %H:%M")
+        return time
 
-    def create():
+    def new_course(self):
         """Create the course folders notes, slides"""
-        os.mkdir(directory)
-        os.mkdir(directory + "/notes")
-        os.mkdir(directory + "/slides")
+        os.mkdir(self.directory)
+        os.mkdir(self.directory + "/notes")
+        os.mkdir(self.directory + "/slides")
 
-        header_file = open(os.getcwd() + "course_layout.md", "r")
+        header_file = open(os.getcwd() + "/course_layout.md", "r")
         header = header_file.read()
         header_file.close()
 
-        note_in = open(note_file)
-        note_in.write(header.format(name.title(), current_time()))
+        note_in = open(self.note_file, "w")
+        note_in.write(header.format(self.name.title(), self.current_time()))
         note_in.close()
 
-    def work():
+    def work(self):
         """Appends date and time to notes file and opens it using VIM"""
-        work_layout_file = open(os.getcwd() + "work_layout.md", "r")
+        work_layout_file = open(os.getcwd() + "/work_layout.md", "r")
         header = work_layout_file.read()
         work_layout_file.close()
 
-        note_file = open(directory + "/notes/" + name + ".md", "a")
-        note_file.write(header.format(current_time()))
+        note_file = open(self.directory + "/notes/" + self.name + ".md", "a")
+        note_file.write(header.format(self.current_time()))
         note_file.close()
 
-        subprocess.run("vim " + note_file)
+        subprocess.call(["vim", self.note_file])
 
-    def copy_from_download():
+    def copy_from_download(self):
         """Copys all files from download folder to slides"""
         pass
--- uni.py
+++ uni.py
@@ -2,60 +2,83 @@
 # coding=utf-8
 
 import os
+import sys
+
+#course.py module
 import course
 
 UNI_DIR = os.getenv("HOME") + "/Documents/uni"
-ALL_FOLDERS = os.listdir(UNI-DIR)
-COUSE_FOLDERS = []
+COURSE_FOLDERS = []
+LINE = "-" * 79
+OPTION_LIST = """\
+[l] - list courses
+[c] - create new course
+[w] - work on existing course
+[q] - quit program"""
+
 
 def start():
     """Initiate program.
 
     Also checks if the uni directory exists."""
     if os.path.exists(UNI_DIR):
-        break
+        pass
     else:
         print("""It appears your uni folder does not exist.
         Would you like to create one?[y/n]""")
         user_in = input("Enter command: ")
-        if user_in == "y" or "yes":
+        if user_in == "y":
             os.mkdir(UNI_DIR)
-        elif user_in == "n" or "no":
+        elif user_in == "n":
             print("As you wish sir. Goodbye!")
-            quit()
+            sys.exit()
         else:
             print("Sorry please answer with [y]es or [n]o!")
             start()
 
 def goodbye():
     """Prompt for quitiing."""
-    prompt = "Are you sure you would like to quit sir? [y/n]"
+    print(LINE)
+    print("Are you sure you would like to quit sir? [y/n]")
     user_in = input("Enter command: ")
-    if user_in == y:
+    if user_in == "y":
         quit()
-    elif user_in == n:
-        break
+    elif user_in == "n":
+        pass
     else:
         print("Sorry I don't know that command.")
         goodbye()
 
-def list_courses():
-    for x in COURSE_FOLDERS:
-        print(x)
+def update_folders():
+    """Selects all non hidden folders"""
+    ALL_FOLDERS = os.listdir(UNI_DIR)
+    COURSE_FOLDERS[:] = []
+    for x in ALL_FOLDERS:
+        if not x.startswith("."):
+            COURSE_FOLDERS.append(x)
 
+
+def list_courses():
+    print(LINE)
+    if COURSE_FOLDERS == []:
+        print("No courses have been defined yet.")
+    else:
+        for x in COURSE_FOLDERS:
+            print(x)
+    print(LINE)
+
+print(LINE)
 print("Welcome sir!")
 
 start()
 
-# Only select non hidden folders
-for x in ALL_FOLDERS:
-    if not x.startswith(".")
-        COURSE_FOLDERS.append(x)
+#Store all folder names
+update_folders()
 
 # Create a Course object for each folder in uni
 COURSES = {}
-for x in COURSE_FOlDERS:
-    COURSES[x] = Course(UNI_DIR, x)
+for x in COURSE_FOLDERS:
+    COURSES[x] = course.Course(UNI_DIR, x)
 
 while True:
     print("How can I help you sir?")
@@ -64,23 +87,22 @@ while True:
         list_courses()
     elif USER_IN == "c":
         COURSE_NAME = input("Enter course name: ")
-        COURSES[COURSE_NAME] = Course(UNI_DIR, COURSE_NAME)
+        COURSES[COURSE_NAME] = course.Course(UNI_DIR, COURSE_NAME)
+        COURSES[COURSE_NAME].new_course()
+        update_folders()
     elif USER_IN == "w":
         print("Which course would you like to work on?")
         list_courses()
         WORK_ON = input("Enter course name: ")
-        for x in COURSES[x]:
+        for x in COURSES:
             if x == WORK_ON:
                 COURSES[x].work()
     elif USER_IN == "q":
         goodbye()
     else:
-        print("Sorry I don't know that command sir."
-        print("""[l] - list courses
-        [c] - create new course
-        [w] - work on existing course
-        [q] - quit program"""
-
-
+        print(LINE)
+        print("Sorry I don't know that command sir.")
+        print(OPTION_LIST)
+        print(LINE)
 
 
